9df3897e807ab11b925771e4594f0d2f89716ae1
junh-park <jhpar25@hotmail.com>
2021-11-30 23:18:26 +1100

    first commit

---
+++ src/main/java/com/jun/tournament/TournamentDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.jun.tournament;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TournamentDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TournamentDashboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jun/tournament/TournamentDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jun.tournament;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TournamentDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
